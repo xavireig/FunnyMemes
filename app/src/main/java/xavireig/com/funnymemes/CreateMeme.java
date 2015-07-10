86fdf450750a3e1bd7089b8434aeb8e85f59901e
Xavier Reig <xavier.reig@gmail.com>
2015-07-10 10:43:10 +0200

    Initial commit

---
+++ app/src/main/java/xavireig/com/funnymemes/CreateMeme.java
@@ -0,0 +1,259 @@
+package xavireig.com.funnymemes;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ShareActionProvider;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class CreateMeme extends Activity {
+    Bitmap originalBitmap, image;
+    ImageView ivMeme;
+    EditText txtText1, txtText2;
+    ShareActionProvider mShareActionProvider;
+    TextPaint paint;
+    int defaultHintColor;
+    int idCurrentImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creatememe);
+        //image view
+        ivMeme = (ImageView) findViewById(R.id.ivMeme);
+
+        txtText1 =(EditText) findViewById(R.id.txtText1);
+        txtText2 =(EditText) findViewById(R.id.txtText2);
+        defaultHintColor = txtText1.getCurrentHintTextColor();
+        idCurrentImage = R.drawable.julio_bg3;
+    }
+
+    private void error(String err) {
+        Toast.makeText(this, err, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu.
+        // Adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider = (ShareActionProvider)shareItem.getActionProvider();
+        }
+
+        return true;
+    }
+
+    public void onGenerateButtonClick(View v) {
+        prepareBitmaps();
+        if (txtText1.getText().toString().compareTo("") != 0 && txtText2.getText().toString().compareTo("") != 0) {
+            // reset colors from error
+            txtText1.setHintTextColor(defaultHintColor);
+            txtText2.setHintTextColor(defaultHintColor);
+            // hide keyboard
+            InputMethodManager imm = (InputMethodManager) getSystemService(Service.INPUT_METHOD_SERVICE);
+            EditText myEditText = (EditText) findViewById(R.id.txtText1);
+            imm.hideSoftInputFromWindow(myEditText.getWindowToken(), 0);
+            Button bt = (Button) findViewById(R.id.btnGenerate);
+            bt.requestFocus();
+
+            //loading original bitmap again (undoing all editing)
+            image = originalBitmap.copy(Bitmap.Config.RGB_565, true);
+            ivMeme.setImageBitmap(image);
+            String topText = txtText1.getText().toString().toUpperCase();
+            String bottomText = txtText2.getText().toString().toUpperCase();
+
+            //function called to perform drawing
+            createImage(topText, bottomText);
+
+            setShareIntent();
+        }
+        else {
+            if (txtText1.getText().toString() != "") txtText1.setHintTextColor(Color.RED);
+            if (txtText2.getText().toString() != "") txtText2.setHintTextColor(Color.RED);
+            error("Type in some text");
+        }
+    }
+
+    public void onChangeImageButtonClick(View v) {
+        Intent intent = new Intent(this, FullScreenViewActivity.class);
+        startActivityForResult(intent, 1);
+        overridePendingTransition(R.anim.animation_splash, R.anim.animation_splash2);
+    }
+
+    private void prepareBitmaps () {
+        // prepare the bitmaps that will be needed to operate
+
+        //dimentions x,y of device to create a scaled bitmap having similar dimentions to screen size
+        int height = ivMeme.getHeight();
+        int width = ivMeme.getWidth();
+
+        //loading bitmap from drawable
+        originalBitmap = BitmapFactory.decodeResource(getResources(), idCurrentImage);
+        //scaling of bitmap
+        originalBitmap = Bitmap.createScaledBitmap(originalBitmap, width, height, false);
+
+        //creating anoter copy of bitmap to be used for editing
+        image = originalBitmap.copy(Bitmap.Config.RGB_565, true);
+    }
+
+    private void setShareIntent() {
+        if (mShareActionProvider != null) {
+
+            File sdCardDirectory = Environment.getExternalStorageDirectory();
+            File imageFile = new File(sdCardDirectory,"meme.png");
+
+            // Encode the file as a PNG image.
+            FileOutputStream outStream;
+            try {
+                outStream = new FileOutputStream(imageFile);
+                image.compress(Bitmap.CompressFormat.PNG, 25, outStream);
+                outStream.flush();
+                outStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("image/png");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_STREAM, Uri.fromFile(imageFile));
+
+            // Make sure the provider knows
+            // it should work with that Intent
+            mShareActionProvider.setShareIntent(sharingIntent);
+        }
+    }
+
+    public Bitmap createImage(String topText, String bottomText){
+        float x, y = 0.0f;
+        //canvas object with bitmap image as constructor
+        Canvas canvas = new Canvas(image);
+        x = canvas.getWidth();
+        y = canvas.getHeight();
+
+        String fontPath = "impact.ttf";
+        Typeface tf = Typeface.createFromAsset(getAssets(), fontPath);
+        paint = new TextPaint();
+        paint.setTypeface(tf);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeMiter(5);
+        paint.setStrokeWidth(18);
+        paint.setTextSize(150);
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setColor(Color.BLACK);
+
+        canvas.save();
+        StaticLayout layoutTop = new StaticLayout(""+topText, paint, canvas.getWidth(), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+        canvas.translate(x / 2, 0);
+        layoutTop.draw(canvas);
+        canvas.restore();
+
+        canvas.save();
+        StaticLayout layoutBottom = new StaticLayout(""+bottomText, paint, canvas.getWidth(), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+        int textHeight = layoutBottom.getHeight();
+        canvas.translate(x / 2, y - textHeight);
+        layoutBottom.draw(canvas);
+        canvas.restore();
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+
+        canvas.save();
+        StaticLayout layoutTop2 = new StaticLayout(""+topText, paint, canvas.getWidth(), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+        canvas.translate(x / 2, 0);
+        layoutTop2.draw(canvas);
+        canvas.restore();
+
+        canvas.save();
+        StaticLayout layoutBottom2 = new StaticLayout(""+bottomText, paint, canvas.getWidth(), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+        textHeight = layoutBottom2.getHeight();
+        canvas.translate(x / 2, y - textHeight);
+        layoutBottom2.draw(canvas);
+        canvas.restore();
+
+        ivMeme.setImageBitmap(image);
+
+        return image;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode) {
+            case (1) : {
+                if (resultCode == Activity.RESULT_OK) {
+                    String newText = data.getStringExtra("picture_number");
+                    idCurrentImage = this.getResources().getIdentifier("meme_bg" + newText, "drawable", this.getPackageName());
+                    ivMeme.setImageResource(idCurrentImage);
+                }
+                break;
+            }
+        }
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_create, container, false);
+            return rootView;
+        }
+    }
+
+    public static class AdFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_ad_create, container, false);
+        }
+
+        @Override
+        public void onActivityCreated(Bundle bundle) {
+            super.onActivityCreated(bundle);
+            AdView mAdView = (AdView) getView().findViewById(R.id.adViewCreate);
+            AdRequest adRequest = new AdRequest.Builder().build();
+            mAdView.loadAd(adRequest);
+        }
+    }
+}
---
+++ app/src/main/java/xavireig/com/funnymemes/Main.java
@@ -0,0 +1,296 @@
+package xavireig.com.funnymemes;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.ShareActionProvider;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
+
+
+public class Main extends Activity {
+
+    ShareActionProvider mShareActionProvider;
+    ImageView iv;
+    Bitmap bitmap;
+    int total_images;
+    private Boolean exit = false;
+
+    MyThread mThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // starting thread to allow it to be created before posting message
+        mThread = new MyThread();
+        mThread.start();
+
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setProgressBarIndeterminateVisibility(false);
+        // Create global configuration and initialize ImageLoader with this config
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
+        ImageLoader.getInstance().init(config);
+        setContentView(R.layout.activity_main);
+        StrictMode.enableDefaults();
+        total_images = 0;
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.julio_bg);
+        // async way to download data
+        mThread.handler.post(new Runnable() {
+
+            @Override
+            public void run() {
+                getData();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (exit)
+            this.finish();
+        else {
+            Toast.makeText(this, "Again to exit",
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    exit = false;
+                }
+            }, 3 * 1000);
+
+        }
+
+    }
+
+    public void onButtonButtonClicked(View view) {
+        ImageLoader il = ImageLoader.getInstance();
+        iv = (ImageView)findViewById(R.id.imageView);
+        // Select random image
+        Random r = new Random();
+        int pic = r.nextInt(total_images) + 1;
+        String imageUri = "http://xavy.net63.net/memes/" + pic + ".jpg";
+        il.displayImage(imageUri, iv, null, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                // start progress bar
+                setProgressBarIndeterminateVisibility(true);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                // stop progress bar
+                setProgressBarIndeterminateVisibility(false);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                iv.setBackgroundDrawable(null);
+                // stop progress bar
+                setProgressBarIndeterminateVisibility(false);
+                bitmap = loadedImage;
+                setShareIntent();
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+
+            }
+        });
+
+    }
+
+    public void onCreateButtonClicked(View view) {
+        Intent intent = new Intent(this, CreateMeme.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.animation_splash, R.anim.animation_splash2);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu.
+        // Adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        // Access the Share Item defined in menu XML
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        // Access the object responsible for
+        // putting together the sharing submenu
+        if (shareItem != null) {
+            mShareActionProvider = (ShareActionProvider)shareItem.getActionProvider();
+        }
+
+        // Create an Intent to share your content
+        setShareIntent();
+
+        return true;
+    }
+
+    private void setShareIntent() {
+        if (mShareActionProvider != null) {
+
+            File sdCardDirectory = Environment.getExternalStorageDirectory();
+            File image = new File(sdCardDirectory,"meme.png");
+
+            // Encode the file as a PNG image.
+            FileOutputStream outStream;
+            try {
+                if (bitmap != null) {
+                    outStream = new FileOutputStream(image);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 25, outStream);
+                    outStream.flush();
+                    outStream.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("image/png");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_STREAM, Uri.fromFile(image));
+
+            // Make sure the provider knows
+            // it should work with that Intent
+            mShareActionProvider.setShareIntent(sharingIntent);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.menu_item_share) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getData() {
+
+        String result = "";
+        InputStream isr = null;
+
+        try {
+            URL url = new URL("http://xavy.net63.net/memes/index.php");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            isr = new BufferedInputStream(urlConnection.getInputStream());
+
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(isr, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                if ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                isr.close();
+
+                result = sb.toString();
+            } catch (Exception e) {
+                Log.e("log_tag", "Error converting result " + e.toString());
+            }
+
+            urlConnection.disconnect();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //parse json data
+        try {
+            total_images = Integer.parseInt(result);
+        } catch (Exception e) {
+            Log.e("log_tag", "Couldn't set text, damnit.");
+        }
+    }
+
+    private class BackgroundOperation extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {}
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
+
+        /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    public static class AdFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_ad, container, false);
+        }
+
+        @Override
+        public void onActivityCreated(Bundle bundle) {
+            super.onActivityCreated(bundle);
+            final AdView mAdView = (AdView) getView().findViewById(R.id.adView);
+            final AdRequest adRequest = new AdRequest.Builder().build();
+            mAdView.loadAd(adRequest);
+        }
+    }
+}
